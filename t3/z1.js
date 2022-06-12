a51086dfa193cf63f2d09dda27893b39f58987b9
Slsanya <93423322+Slsanya@users.noreply.github.com>
2022-06-12 13:46:31 +0300

    Add files via upload

---
+++ t3/z1.js
@@ -0,0 +1,18 @@
+﻿/*alert(`hi`);
+*/
+let imya = `sasha`;
+let u = `43`;
+let name = prompt(`як звати президента україни?`);
+
+if(name == `володимер`){
+    alert(`true`);
+}else{
+    alert(`false`);	
+}
+let age = confirm(` яка ціна російського немовля?`);
+
+if(age){
+    alert(`не дорого 30грн`);
+}else{
+    alert(`дорого 300грн`);
+}
